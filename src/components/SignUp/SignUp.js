650934760c6957deddf08e62bc8750e49060d906
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-11 16:37:25 +0600

    Added user-password signup function.

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './SignUp.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const [createUserWithEmailAndPassword, user] =
+    useCreateUserWithEmailAndPassword(auth);
 
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
@@ -20,12 +26,22 @@ const SignUp = () => {
     setConfirmPassword(event.target.value);
   };
 
+  if (user) {
+    navigate('/home');
+  }
+
   const handleCreateUser = (event) => {
     event.preventDefault();
     if (password !== confirmPassword) {
       setError('Your two password did not matched!');
       return;
     }
+
+    if (password.length < 6) {
+      setError('Password minimum 6 character in length!');
+    }
+
+    createUserWithEmailAndPassword(email, password);
   };
 
   return (
